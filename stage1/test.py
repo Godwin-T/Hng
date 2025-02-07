a303dad0f2be498cfb3a121360a260ff509c20a9
Godwin-T <godwinolagunju0@gmail.com>
2025-02-07 04:31:47 +0100

    remove print

--- stage1/test.py
+++ stage1/test.py
@@ -1,6 +1,8 @@
 import requests
 
-url = "http://127.0.0.1:5000/api/classify-number?number=371"
 
+
+number = int(input("Enter a number: \n"))
+url = f"http://ec2-35-87-181-212.us-west-2.compute.amazonaws.com/api/classify-number?number={number}"
 response = requests.get(url)
 print(response.text)
